d04c6e2420e96b38147f8fd1b5fe73c482ed530d
OO <O.O>
2016-04-26 23:29:38 +0300

    Class edits

---
+++ olioharjoitustyo/olioharjoitustyo/tyyppi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace olioharjoitustyo
+{
+    class tyyppi
+    {
+        public string Tyyppi { get; set; }
+        public double Matka { get; set; }
+        public double Kesto { get; set; }
+        public string Kuvaus { get; set; }
+    }
+}
