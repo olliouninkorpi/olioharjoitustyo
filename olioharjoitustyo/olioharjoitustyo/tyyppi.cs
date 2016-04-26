a926baad8ecf0209f0963e58a7ea60751740d429
OO <O.O>
2016-04-26 23:43:14 +0300

    Class edits

--- olioharjoitustyo/olioharjoitustyo/tyyppi.cs
+++ olioharjoitustyo/olioharjoitustyo/tyyppi.cs
@@ -8,9 +8,24 @@ namespace olioharjoitustyo
 {
     class tyyppi
     {
+        private readonly double MaxKesto = 7200;
         public string Tyyppi { get; set; }
         public double Matka { get; set; }
-        public double Kesto { get; set; }
+        private double kesto;
+        public double Kesto {
+                get
+            {
+                return kesto;
+            }
+            set
+            {
+                if (value <= MaxKesto) kesto = value;
+                else
+                {
+                    Console.WriteLine("Virheellinen kesto. Syötä pienempi luku.");
+                    kesto = 0;
+                }
+            }
         public string Kuvaus { get; set; }
     }
 }
